7d0348ec924666e6f79feba0a20dadab8005db5e
Harsh_324 <harshravi324@gmail.com>
2023-01-28 23:42:53 +0530

    Some changes done

--- Tree/z.cpp
+++ Tree/z.cpp
@@ -47,6 +47,91 @@ void Postorder(Node* Tree)
     cout << Tree->data <<" ";
 }
 
+vector<vector<int>> Level_order(Node* Tree)
+{
+    vector<vector<int>> Res;
+
+    if(NULL == Tree)
+        return Res;
+    
+    queue<Node*> Q;
+    Q.push(Tree);
+
+    while(Q.empty() != true)
+    {
+        int Sz = Q.size();
+        vector<int> V;
+        for(int i = 0; i < Sz; i++)
+        {
+            Node* node = Q.front();
+            Q.pop();
+            if(Tree->left != NULL)
+                Q.push(Tree->left);
+            if(Tree->right != NULL)
+                Q.push(Tree->right);
+            
+            V.push_back(node->data);
+        }
+    }
+    return Res;
+}
+
+
+vector<int> iter_preorder(Node* Tree)
+{
+    vector<int> Res;
+    stack<Node*> St;
+
+    
+    Node* node = Tree;
+
+    while(true)
+    {
+        if(NULL == node)
+        {
+            St.push(node);
+            node = node->left;
+        }
+        else{
+            if(St.empty() == true)
+                break;
+            node = St.top();
+            St.pop();
+            Res.push_back(node->data);
+            node = node->right;
+        }
+    }
+    return Res;
+}
+
+
+
+vector<int> iter_ineorder(Node* Tree)
+{
+    vector<int> Res;
+
+    if(NULL == Tree)
+        return Res;
+    
+    stack<Node*> St;
+    St.push(Tree);
+
+    while(St.empty() != true)
+    {
+        Tree = St.top();
+        St.pop();
+        Res.push_back(Tree->data);
+
+        if(NULL == Tree->right)
+            St.push(Tree->right);
+        
+        if(NULL == Tree->left)
+            St.push(Tree->left);
+    }
+    return Res;
+}
+
+
 
 int main()
 {
